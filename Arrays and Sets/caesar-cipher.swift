b276f6b4d94251c88bca3de82987264672e5a4d2
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-03-25 20:23:15 +0700

    add: Carsar's Cipher

---
+++ Arrays and Sets/caesar-cipher.swift
@@ -0,0 +1,16 @@
+let alphabet: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var secretMessage: String = "Cipher"
+
+var message = Array(secretMessage.lowercased())
+
+for i: Int in 0..<message.count {
+  for j: Int in 0..<alphabet.count {
+    if message[i] == alphabet[j] {
+      message[i] = alphabet[(j+3) % 26]
+      break
+    }
+  }
+}
+
+print(String(message))
