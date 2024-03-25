af8dec89195c2c29882b2dbd6b493cb67a9e97c4
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-03-25 12:46:37 +0700

    add: 99 bottles of milk

---
+++ Loops/99bottles.swift
@@ -0,0 +1,14 @@
+var numBottles: Int = 99
+
+while numBottles > 0 {
+  print("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk! You take one down and pass it around...")
+  print("\n")
+  numBottles -= 1
+  print("\(numBottles) bottles of milk on the wall!")
+  print("\n")
+}
+
+print("0 bottles of milk on the wall, 0 bottles of milk! You take one down and pass it around...")
+print("Go to the store and buy some more")
+print("\n")
+print("99 bottles of milk on the wall!")
