f554e3b17c8e92e082b619a2692149d37d45f9a8
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-03-25 12:36:33 +0700

    add: whale talk

---
+++ Loops/whaletalk.swift
@@ -0,0 +1,18 @@
+var input: String = "The quick brown fox jump over a lazy dog"
+var output: String = ""
+
+for char in input {
+  let lowerChar: String = char.lowercased()
+  switch lowerChar {
+  case "a","i","o":
+    output += lowerChar.uppercased()
+  case "e":
+    output += "EE"
+  case "u":
+    output += "UU"
+  default:
+    continue
+  }
+}
+
+print(output)
