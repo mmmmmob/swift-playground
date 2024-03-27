a59e4b51f3b3b2452922dbd157800426f40ab9d7
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-03-27 23:45:46 +0700

    add: Pokedex

---
+++ 08_Classes/pokedex.swift
@@ -0,0 +1,40 @@
+class Pokemon {
+  var num = 0
+  var name = ""
+  var type = [""]
+  var ability = [""]
+
+  init(num: Int, name: String, type: [String], ability: [String]) {
+    self.num = num
+    self.name = name
+    self.type = type
+    self.ability = ability
+  }
+
+  func displayInfo() {
+    print("\(self.name) (#\(self.num)) : is a \(self.type) pokemon that has \(self.ability) as their signature move!\n")
+  }
+}
+
+var fushigidane = Pokemon(num: 1, name: "フシギダネ", type: ["Grass 🌱","Poison 💀"], ability: ["Overgrow"])
+
+var hitokage = Pokemon(num: 4, name: "ヒトカゲ", type: ["Fire 🔥"], ability: ["Blaze"])
+
+var zenigame = Pokemon(num: 7, name: "ゼニガメ", type: ["Water 💧"], ability: ["Torrent"])
+
+fushigidane.displayInfo()
+hitokage.displayInfo()
+zenigame.displayInfo()
+
+class GigantamaxPokemon: Pokemon {
+  var location = ""
+
+  init(num: Int, name: String, type: [String], ability: [String], location: String) {
+    self.location = location
+    super.init(num: num, name: name, type: type, ability: ability)
+  }
+}
+
+var rizadon = GigantamaxPokemon(num: 6, name: "リザードン", type: ["Fire 🔥"], ability: ["Blaze"], location: "Lake of Outrage")
+
+rizadon.displayInfo()
