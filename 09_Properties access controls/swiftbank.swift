cc18187b8fc84677ae42f051cf2a1f82be351c19
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-03-28 21:49:28 +0700

    feat: finish basic Swiftbank

--- 09_Properties access controls/swiftbank.swift
+++ 09_Properties access controls/swiftbank.swift
@@ -13,29 +13,58 @@ struct SwiftBank {
     } else { return false }
   }
 
-  private var balance: Double = 0
-
-  // type property
-  static let depositBonusRate = 0.01
-
-  private func finalDepositWithBonus(fromInitialDeposit deposit: Double) -> Double {
-    return deposit += deposit * SwiftBank.depositBonusRate
+  private var balance: Double = 0 {
+    willSet(newBalance) { // property observer on var balance
+      if newBalance < 100 {
+        displayLowBalanceMessage()
+      }
+    }
   }
 
-  // main method mutate var balance
+  private func displayLowBalanceMessage() {
+    print("Alert: Your balance is under $100")
+  }
+
+  // type property
+  static let depositBonusRate: Double = 0.01
+
+  private func finalDepositWithBonus(fromInitialDeposit deposit: Double) -> Double {
+    return deposit + deposit * SwiftBank.depositBonusRate
+    
+  }
+
+  // ------ main method ** refactor as extension later? ** ----- //
+
+  // deposit method (mutate var balance)
   mutating func makeDeposit(ofAmount depositAmount: Double) {
     let depositWithBonus = finalDepositWithBonus(fromInitialDeposit: depositAmount)
-    print("Making a deposit of $\(depositAmount) with a bonus rate of \(SwiftBank.depositBonusRate). The final amount deposited is $\(depositWithBonus)")
+    print("Making a deposit of $\(depositAmount) with a bonus rate of \(SwiftBank.depositBonusRate). The final amount deposited is $\(depositWithBonus)\n")
     balance += depositWithBonus
   }
 
-  // login method
+  // show balance method
   func displayBalance(usingPassword password: String) {
     if isValid(password) {
-      print("Your current balance is $\(balance)")
+      print("Your current balance is $\(balance)\n")
     } else {
-      print("Error: Invalid password. Cannot retrieve balance.")
+      print("Error: Invalid password. Cannot retrieve balance.\n")
+      return
+    }
+  }
+
+  // withdrawal method (mutate var balance)
+  mutating func makeWithdrawal(ofAmount withdrawalAmount: Double, usingPassword password: String) {
+    if isValid(password) {
+      balance -= withdrawalAmount
+      print("Making a $\(withdrawalAmount) withdrawal.\n")
+    } else {
+      print("Error: Invalid password. Cannot make withdrawal.\n")
       return
     }
   }
 }
+
+var myAccount = SwiftBank(password: "1234pass", initiaDeposit: 500)
+myAccount.makeDeposit(ofAmount: 50)
+myAccount.makeWithdrawal(ofAmount: 100, usingPassword: "1234pass")
+myAccount.displayBalance(usingPassword: "1234pass")
