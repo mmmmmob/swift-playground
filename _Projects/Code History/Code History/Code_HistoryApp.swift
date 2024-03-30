d9cb8c437a614ded94762e9b39bb4d7fc7cb18d4
Theppitak M <141404845+mmmmmob@users.noreply.github.com>
2024-03-30 10:53:21 +0700

    add: Code History Xcode project

---
+++ _Projects/Code History/Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Theppitak M. on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
