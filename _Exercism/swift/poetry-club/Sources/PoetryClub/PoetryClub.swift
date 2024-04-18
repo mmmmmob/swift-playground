1f3100e62dda527babfddfbbdfa763c640ac970c
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-04-18 12:49:33 +0700

    add: initial code for Poetry Club

---
+++ _Exercism/swift/poetry-club/Sources/PoetryClub/PoetryClub.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+// split a string into individual lines
+func splitOnNewlines(_ poem: String) -> [String] {
+  
+}
+
+// get the first letter of sentence
+func firstLetter(_ line: String) -> Character {
+}
+
+// capitalize a word
+func capitalize(_ phrase: String) -> String {
+}
+
+// trim a sentence
+func trimSentence(_ line: String) -> String {
+}
+
+// get the last letter of a sentence
+func lastLetter(_ line: String) -> Character {
+}
+
+// be polite with ', please' insertion after a word
+func backDoorPassword(_ phrase: String) -> String {
+}
+
+// get the i letter of a  sentence
+func ithLetter(_ line: String, i: Int) -> Character {
+}
+
+// shout the anwer with capital letters
+func secretRoomPassword(_ phrase: String) -> String {
+}
