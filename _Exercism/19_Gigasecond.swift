648ea8ae85fa31c58104b00c369979dec8511ee1
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-04-22 15:41:12 +0700

    feat: finish gigasecond

---
+++ _Exercism/19_Gigasecond.swift
@@ -0,0 +1,14 @@
+/*
+- Your task is to determine the date and time one gigasecond after a certain date.
+- A gigasecond is one thousand million seconds. That is a one with nine zeros after it.
+- If you were born on January 24th, 2015 at 22:00 (10:00:00pm), then you would be a gigasecond old on October 2nd, 2046 at 23:46:40 (11:46:40pm).
+*/
+
+import Foundation
+
+func gigasecond(from: Date) -> Date {
+  let futureDate = Date(timeInterval: 1_000_000_000, since: from)
+  return futureDate
+}
+
+print(gigasecond(from: Date.now))
