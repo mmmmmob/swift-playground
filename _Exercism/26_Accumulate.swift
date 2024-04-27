eb5705ca51b46fca87ebdc6b55274fc4f1ff95e8
Theppitak M <141404845+mmmmmob@users.noreply.github.com>
2024-04-27 12:00:26 +0700

    add: initial code for accumulate

---
+++ _Exercism/26_Accumulate.swift
@@ -0,0 +1,16 @@
+/*
+  Implement the accumulate operation, which, given a collection and an operation to perform on each element of the collection, returns a new collection containing the result of applying that operation to each element of the input collection.
+  
+  Given the collection of numbers:
+    1, 2, 3, 4, 5
+
+  And the operation:
+    square a number (x => x * x)
+
+  Your code should be able to produce the collection of squares:
+    1, 4, 9, 16, 25
+*/
+
+struct Accumulate {
+  
+}
