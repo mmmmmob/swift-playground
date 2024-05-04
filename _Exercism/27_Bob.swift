a4849d92f7e0ee815fc8c18926b4445238e3ed85
Theppitak M <141404845+mmmmmob@users.noreply.github.com>
2024-05-04 19:53:31 +0700

    add: complete bob

--- _Exercism/27_Bob.swift
+++ _Exercism/27_Bob.swift
@@ -11,16 +11,24 @@ import Foundation
 
 class Bob {
   static func response(_ message: String) -> String {
-    if message.allSatisfy({$0.isUppercase}) && message.last == "?" {
-      return "Calm down, I know what I'm doing!"
-    } else if message.contains("\t") || message.count == 0 {
-      return "Fine. Be that way!"
-    } else if message.allSatisfy({$0.isUppercase}) {
-      return "Whoa, chill out!"
-    } else if message.last == "?" {
-      return "Sure."
+    // filter out whitespaces and symbols for next steps
+    let truncateAllSpace: String = message.filter{!$0.isWhitespace}
+    let truncateSymbol: String = truncateAllSpace.filter{!$0.isPunctuation}.filter{!$0.isSymbol}.filter{!$0.isNumber}
+
+    // check if all character is uppercase or not with filter method
+    let checkUppercased: String = truncateSymbol.filter{$0.isUppercase}
+    var isUppercase: Bool = false
+
+    if checkUppercased == truncateSymbol {
+      isUppercase = true
     }
-    else { return "Whatever."}
+
+    // return string by each case
+    if truncateAllSpace.count == 0 { return "Fine. Be that way!"}
+    else if isUppercase && checkUppercased.count != 0 && truncateAllSpace.last == "?" { return "Calm down, I know what I'm doing!" }
+    else if truncateAllSpace.last == "?" { return "Sure."} 
+    else if isUppercase && checkUppercased.count != 0 { return "Whoa, chill out!" }
+    else { return "Whatever." }
   }
 }
 
@@ -29,4 +37,7 @@ print(Bob.response("WHAT'S GOING ON?")) // Calm down, I know what I'm doing.
 print(Bob.response("1, 2, 3 GO!")) // Whoa, chill out!
 print(Bob.response("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!")) // Whoa, chill out!
 print(Bob.response("          ")) // Fine. Be that way!
-print(Bob.response("Okay if like my  spacebar  quite a bit?   ")) // Sure. 
+print(Bob.response("Okay if like my  spacebar  quite a bit?   ")) // Sure.
+print(Bob.response("It's OK if you don't want to go work for NASA.")) // Whatever.
+print(Bob.response("Wait! Hang on. Are you going to be OK?")) // Sure.
+print(Bob.response(":) ?")) // Sure.
