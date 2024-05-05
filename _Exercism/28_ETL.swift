e311752c8b0529af190597eddf6f0ea53a4cc719
Theppitak M <141404845+mmmmmob@users.noreply.github.com>
2024-05-05 11:48:34 +0700

    feat: finish ETL

--- _Exercism/28_ETL.swift
+++ _Exercism/28_ETL.swift
@@ -19,7 +19,17 @@
 
 class ETL {
   static func transform(_ old: [String: [String]]) -> [String: Int] {
-    // Write your code for the 'Etl' exercise in this file.
+    var result: [String: Int] = [:]
+    
+    for (score, alphabet) in old {
+      let newScore = Int(score)
+      for each in alphabet {
+        let newAlphabet = each.lowercased()
+        result.updateValue(newScore!, forKey: newAlphabet)
+      }
+    }
+
+    return result
   }
 }
 
