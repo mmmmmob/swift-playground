b14f2b7c5e1cf1041cc8fde01192d75a3908e8d2
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-05-11 00:05:40 +0700

    feat: finish Grains

--- _Exercism/30_Grains.swift
+++ _Exercism/30_Grains.swift
@@ -10,12 +10,42 @@
     - the total number of grains on the chessboard
 */
 
+import Foundation
+
+// declare Error enum for throwing function
+enum GrainsError: Error {
+    case inputTooHigh
+    case inputTooLow
+  }
 struct Grains {
   static func square(_ num: Int) throws -> UInt64 {
     
+    // error handling with throw
+    if num < 1 {
+      throw GrainsError.inputTooLow
+    } else if num > 64 {
+      throw GrainsError.inputTooHigh
+    }
+
+    let result = UInt64(pow(2, Double(num - 1)))
+
+    return result
+  }
+
+  static var total: UInt64 {
+    var sum: UInt64 = 0
+    for i in 1...64 {
+      sum += try! Grains.square(i) // use try! because we know for the fact that there can't be error throws at us on runtime
+    }
+    return UInt64(sum)
   }
 }
 
-print(Grains.square(32)) // 2_147_483_648
-print(Grains.square(4)) // 8
-print(Grains.square(3)) // 4
+// use try every usage of throwing function
+print(try(Grains.square(32))) // 2_147_483_648
+print(try(Grains.square(4))) // 8
+print(try(Grains.square(3))) // 4
+print(try(Grains.square(16))) // 32768
+//print(try(Grains.square(-1))) // error: GrainsError.inputTooLow
+//print(try(Grains.square(66))) // error: GrainsError.inputTooHigh
+print(Grains.total) // 18_446_744_073_709_551_615
