58338d5814e2d8e285ad0aa6459e9f468d95a8d3
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-05-14 18:43:58 +0700

    feat: finish Isogram

--- _Exercism/32_Isogram.swift
+++ _Exercism/32_Isogram.swift
@@ -11,9 +11,27 @@
 
   The word isograms, however, is not an isogram, because the s repeats.
 */
+import Foundation
 
 func isIsogram(_ string: String) -> Bool {
-  
+  var result: Bool = true
+
+  if string.count == 0 {
+    return result
+  } else {
+    for i in 0...string.count - 1 {
+      var string = string.lowercased()
+      let index = string.index(string.startIndex, offsetBy: i)
+
+      if string[index] == " " || string[index] == "-" { continue }
+
+      let checkString = string.remove(at: index)
+      if string.contains(checkString) {
+        result = false
+      }
+    }
+  }
+  return result
 }
 
 print(isIsogram("isograms")) // false
