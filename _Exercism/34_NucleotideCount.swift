39bfa198860a296a2347dea1053c8e5d9c4f7647
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-05-17 10:26:24 +0700

    add: initial instruction and codebase for Nucleotide Count

---
+++ _Exercism/34_NucleotideCount.swift
@@ -0,0 +1,22 @@
+/*
+  Given a string representing a DNA sequence, count how many of each nucleotide is present. If the string contains characters that aren't A, C, G, or T then it is invalid and you should signal an error.
+
+  For example:
+    "GATTACA" -> 'A': 3, 'C': 1, 'G': 1, 'T': 2
+    "INVALID" -> error
+*/
+
+class DNA {
+  var strand: String
+
+  init(strand: String) {
+    self.strand = strand
+  }
+
+  func count() -> [String : Int] {
+    return [self.strand : 0]
+  }
+}
+
+let dna = DNA(strand: "AAAA")
+print(dna.count())
